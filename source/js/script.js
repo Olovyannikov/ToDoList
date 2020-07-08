451dd57b980f631f2373280a88eb024431ff48df
Olovyannikov <excaliburgyro@gmail.com>
2020-07-08 11:06:40 +0300

    Update

--- source/js/script.js
+++ source/js/script.js
@@ -6,6 +6,7 @@ $(function () {
             var newTask = $('<h3 class="list__head"></h3>');
             var iconCross = $('<span class="list__head-icon" onclick="$(this).closest(\'.list\').remove();">❌</span>')
             var newElement = $('<p class="list__content"></p>');
+            var hideItem = $('<button class="hide-item">▲</button>');
             var task = document.querySelector('#name');
             var description = document.querySelector('#describe');
 
@@ -13,7 +14,16 @@ $(function () {
             newElement.text(description.value);
             $('.column-left').append(newBlock);
 
-            $(newBlock).append(newTask).append(iconCross).append(newElement);
-
+            $(newBlock).append(newTask).append(iconCross).append(hideItem).append(newElement);
+                hideItem.click(function () {
+                        this.classList.toggle('rotate');
+                        if(newElement.is(':visible')) {
+                                newElement.hide();
+                        }else {
+                                newElement.show();
+                        }
+                });
         });
+
 });
+
